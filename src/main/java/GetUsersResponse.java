d50a37dc6872386b1d9c5cec294edc5b9dd6df1d
unknown <kubikweg@gmail.com>
2022-10-31 08:39:59 +0200

    CRUD and model

--- src/main/java/GetUsersResponse.java
+++ src/main/java/GetUsersResponse.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class GetUsersResponse {
+import java.util.ArrayList;
+
+public class GetUsersResponse {
+    public int page;
+    public int per_page;
+    public int total;
+    public int total_pages;
+    public ArrayList<User> data;
+    public SupportResponse support;
 }
